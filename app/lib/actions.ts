fa347006f19b906f1ee02f42ca3a5af4d34d21a2
DenisSkendaj <imdenisskendaj@gmail.com>
2023-12-09 10:21:37 +0000

    To Update fetch AvgPrice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,3 +20,35 @@ export async function authenticate(
         throw error;
     }
 }
+
+
+// TO update below
+
+const fetchAveragePrice = async (symbol) => {
+    try {
+      const response = await fetch(`https://data-api.binance.vision/api/v3/avgPrice?symbol=${symbol}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching average price:', error.message);
+      return null;
+    }
+  };
+  
+  const getAveragePriceEvery3Sec = (symbol) => {
+    setInterval(async () => {
+      const result = await fetchAveragePrice(symbol);
+      if (result) {
+        console.log('Current Average Price:', result);
+        // Do something with the result here
+      }
+    }, 3000); // 3 seconds in milliseconds
+  };
+  
+  // Example usage:
+  const symbolToFetch = 'BTCUSDT'; // Replace with the symbol you want to fetch
+  getAveragePriceEvery3Sec(symbolToFetch);
+  
