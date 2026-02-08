8eeb5110a606688c83aa510d61034b8213c41ced
DenisBytes <denisbytes@gmail.com>
2026-02-08 12:01:08 +0100

    Swap NextAuth for Auth0

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { auth0 } from '@/lib/auth0'
 
-export default NextAuth(authConfig).auth;
+export async function middleware(request: NextRequest) {
+  return await auth0.middleware(request)
+}
 
 export const config = {
-  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.jpg|.*\\.avif|.*\\.svg$).*)'],
-};
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
+}
